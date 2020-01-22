dff859e977b3f89e4e44d462aca224d350305b90
Patrick <sublime@vt.edu>
2020-01-22 02:50:08 -0500

    Use datetime, BloggerRow, and gives full participation report

--- reportOnComments.py
+++ reportOnComments.py
@@ -2,16 +2,17 @@ import os
 import csv
 import sys
 import argparse
+from datetime import datetime
+from collections import Counter
+from collections import defaultdict
 from xml.etree import ElementTree
 
-"""
-Create a report of blogging comment participation
+"""Create a report of blogging comment participation
 
-This script converts WXR file to a number of plain text files.
-WXR stands for "WordPress eXtended RSS", which basically is just a
-regular XML file. This script extracts entries from the WXR file into
-plain text files. Output format: article name prefixed by date for
-posts, article name for pages.
+This script reads an exported WordPress XML file (aka 'WXR').  
+It then finds all comments on all posts and creates a tally
+for the number of comments that each username created on posts.  
+
 Original author: Ruslan Osipov
 Updated by: Arash, Patrick Sullivan
 
@@ -27,47 +28,90 @@ optional arguments:
 
 # TODO: Improve report.  Average comment length. 
 
+OLDEST_POST = datetime(2015, 10, 1)
+
 NAMESPACES = {
         'content': 'http://purl.org/rss/1.0/modules/content/',
         'wp': 'http://wordpress.org/export/1.2/',
         'dc': 'http://purl.org/dc/elements/1.1/',
+        'excerpt': "http://wordpress.org/export/1.2/excerpt/", 
+        'wfw': "http://wellformedweb.org/CommentAPI/",
 }
+
+class BloggerRow():
+    def __init__(self):
+        self.username = None
+        self.postCount = 0
+        self.postTotalLen = 0
+        self.commentCount = 0
+        self.commentTotalLen = 0
+        self.replyCount = 0
+        self.replyTotalLen = 0
 
 def main():
     args = getArgs()
     xmlRoot = args.input
     outputFilename = args.output
 
-    page_counter, post_counter = 0, 0
-    commenters ={}
+    typeCounter = Counter()
+    rows = defaultdict(BloggerRow)
+
     for post in xmlRoot.find('channel').findall('item'):
         post_type = post.find('wp:post_type', namespaces=NAMESPACES).text
-        if post_type not in ('post', 'page'):
+        typeCounter[post_type] += 1
+
+        if post_type != 'post':
             continue
-        content = post.find('content:encoded', namespaces=NAMESPACES).text
-        if content and post_type=='post':
-            creator = str(post.find('dc:creator', namespaces=NAMESPACES).text).lower()
-            date = post.find('wp:post_date', namespaces=NAMESPACES).text
-            #title = post.find('title').text
-            date = date.split(' ')[0].replace('-', '')
-            # title = re.sub(r'[_]+', '_', re.sub(r'[^a-z0-9+]', '_', title.lower()))
-            if int(date) > 20180801:
-                if  creator in commenters:
-                    commenters[creator] += 1
-                else:
-                    commenters[creator] = 1
+
+        title = post.find('title').text
+        creator = str(post.find('dc:creator', namespaces=NAMESPACES).text)
+        dateStr = post.find('wp:post_date', namespaces=NAMESPACES).text
+        date = datetime.fromisoformat(dateStr)
+        postContent = post.find('content:encoded', namespaces=NAMESPACES).text
+
+        if date < OLDEST_POST:
+            continue
+        if postContent is None:
+            continue
+
+        rows[creator].username = creator
+        rows[creator].postCount += 1
+        rows[creator].postTotalLen += len(postContent)
+
+        for comment in post.findall("wp:comment", namespaces=NAMESPACES):
+            commenter = comment.find('wp:comment_author_email', namespaces=NAMESPACES).text
+            commenter = commenter.split('@')[0]
+            commentText = comment.find('wp:comment_content', namespaces=NAMESPACES).text
+            commentParentId = int(comment.find('wp:comment_parent', namespaces=NAMESPACES).text)
+
+            rows[commenter].username = commenter
+            if commentParentId == 0:
+                rows[commenter].commentCount += 1
+                rows[commenter].commentTotalLen += len(commentText)
+            else:
+                rows[commenter].replyCount += 1
+                rows[commenter].replyTotalLen += len(commentText)
+
+    print(typeCounter)
+
     with open(outputFilename,'w', newline='') as f:
         w = csv.writer(f)
-        for kv in commenters.items():
-            print(kv)
-            w.writerow(kv)
+        headRow = True
 
+        for bloggerRow in rows.values():
+            if headRow:
+                colHeaders = vars(bloggerRow).keys()
+                w.writerow(colHeaders)
+                headRow = False
+            rowVals = vars(bloggerRow).values()
+            w.writerow(rowVals)
+            
 
 def validXML(filename):
     try:
         return ElementTree.parse(filename).getroot()
     except ElementTree.ParseError as e:
-        print("Error: Could not XML parse", filename, ":", e, file=sys.stderr)
+        print("Error: Could not parse XML of ", filename, ":", e, file=sys.stderr)
         exit(1)
 
 def getArgs():
